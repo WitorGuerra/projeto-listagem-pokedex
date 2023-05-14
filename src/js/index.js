bd1c1d7661d0dd51907ea24567cfcac0a75cdb70
WituGuerra <131100492+WitorGuerra@users.noreply.github.com>
2023-05-14 11:47:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+alert("Obrigado por Acessar nossa pokedex. No momento ela ainda está sendo desenvolvida. Por enquanto possuimos apenas os pokemons da primeira geração. Aguardem atualizações.");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body")
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+    body.classList.toggle("modo-escuro");
+    
+    if(modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+    } else{
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png"); 
+    }   
+});
